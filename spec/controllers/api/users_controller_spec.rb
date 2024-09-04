34ce98101c6b8c2fd96d066c96737b72f9520d68
chandan kumar <ckumar@kreeti.com>
2024-09-04 19:01:41 +0530

    Write specs

---
+++ spec/controllers/api/users_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Api::UsersController, type: :controller do
+  describe 'POST #register' do
+    context 'with valid attributes' do
+      let(:valid_attributes) do
+        {
+          name: 'Chandan kumar',
+          email: 'chandan.kumar@example.com',
+          role: 'member',
+          password: 'password123'
+        }
+      end
+
+      it 'creates a new user' do
+        post :register, params: valid_attributes
+        expect(response).to have_http_status(201)
+        json_response = JSON.parse(response.body)
+        expect(json_response['name']).to eq('Chandan kumar')
+        expect(json_response['role']).to eq('member')
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:invalid_attributes) do
+        {
+          name: '',
+          email: 'abc@xyz.com',
+          password: 'Password',
+        }
+      end
+
+      it 'does not create a new user' do
+        expect do
+          post :register, params: invalid_attributes
+        end.to_not change(User, :count)
+      end
+    end
+  end
+end
